5d04497ad5b4417678a2443c0e2808270acd0e50
jfandre00 <jfandre@gmail.com>
2025-10-28 08:45:09 -0400

    Add menu and seller category logic to Vendedor
    Introduces a menu-driven interface for adding sellers in ElberthandreapiApplication, with improved input validation for salary and sales quantity. Adds seller categorization logic in Vendedor based on number of sales, and updates output to display the seller's category upon registration.

--- src/main/java/br/edu/infnet/elberthandreapi/ElberthandreapiApplication.java
+++ src/main/java/br/edu/infnet/elberthandreapi/ElberthandreapiApplication.java
@@ -17,50 +17,108 @@ public class ElberthandreapiApplication {
 		
 		System.out.println("Iniciando...");
 		
-		
-		// Instanciar um objeto da classe Vendedor
-		
-		Vendedor vendedorElberth = new Vendedor();	
-		System.out.print("Digite o nome do vendedor: ");
-		vendedorElberth.nome = in.nextLine();  //"Elberth";	
-		
-		System.out.print("Informe o salário do vendedor: ");
-		if(!in.hasNextDouble()) { // validação de entrada, se o usuário digitar um valor inválido - método boolean
-			System.out.println("O salário do vendedor está inválido!!");
-			in.next(); // irá atribuir o valor 0 
+		int opcao = 0;
+		do {
+			System.out.println("1 - Incluir vendedor");
+			System.out.println("9 - Sair");
+			System.out.println("Escolha a opção desejada:");
+			opcao = in.nextInt();
+			in.nextLine(); // <<< limpa o buffer após nextInt
+			switch (opcao) {
+			case 1:
+				// Instanciar um objeto da classe Vendedor
+				Vendedor vendedorElberth = new Vendedor();	
+				System.out.print("Digite o nome do vendedor: ");
+				vendedorElberth.nome = in.nextLine();  //"Elberth";	
+				
+				System.out.print("Informe o salário do vendedor: ");
+				if(!in.hasNextDouble()) { // validação de entrada, se o usuário digitar um valor inválido - método boolean
+					String campo = in.next(); // lê o valor inválido
+					
+					System.out.println("O salário do vendedor está inválido!! [" + campo + "]");
+					in.next(); // irá atribuir o valor 0 
+					
+				} else {
+					// vamos fazer o tratamento para verificar se o valor é menor que zero
+					double salario = in.nextDouble();
+					
+					if(salario < 0) {
+						System.out.println("O salário não pode ser menor que zero!!");
+						// como o valor padrão de double é 0.0, não preciso atribuir nada e o salário já ficará com esse valor
+					} else {
+						vendedorElberth.salario = salario;
+					}
+				}
+				
+				in.nextLine(); // <<< limpa o buffer após nextDouble
+				
+				
+				
+				
+				
+				System.out.print("Informe a quantidade de vendas do vendedor: ");
+				if (!in.hasNextInt()) { // validação de entrada, se o usuário digitar um valor inválido - método boolean
+					System.out.println("A quantidade de vendas do vendedor está inválida!!");
+					in.next(); // irá atribuir o valor 0 
+					
+				} else {
+					
+					int qtdeVendas = in.nextInt();
+					if (qtdeVendas < 0) {
+						System.out.println("A quantidade de vendas está negativa!! ["+ qtdeVendas + "]!!!!");
+						// como o valor padrão de int é 0, não preciso atribuir nada e a qtdeVendas já ficará com esse valor
+					} 
+					else {
+						vendedorElberth.qtdeVendas = qtdeVendas;
+						if (qtdeVendas == 0) {
+							System.out.println("Esse vendedor não teve vendas neste período e não receberá comissão extra!!");
+						}
+					}
+				}
+				vendedorElberth.imprimir();
 			
-		} else {
-			// vamos fazer o tratamento para verificar se o valor é menor que zero
-			double salario = in.nextDouble();
+				break;	
+			case 9:
+				System.out.println("Finalizando o sistema...");
+				break;
 			
-			if(salario < 0) {
-				System.out.println("O salário não pode ser menor que zero!!");
-				// como o valor padrão de double é 0.0, não preciso atribuir nada e o salário já ficará com esse valor
-			} else {
-				vendedorElberth.salario = salario;
+			default:
+				System.out.println("Qualquer outra opção!!");
+				break;
 			}
-		}
+		} while (opcao != 9);
+		
+
 		
 		
-		System.out.print("Informe a quantidade de vendas do vendedor: ");
+		
+		
+	
+		/*
+		int qtdeVendedores = 0;
+		
+		System.out.print("Informe a quantidade de vendedores que deseja cadastrar: ");
 		if (!in.hasNextInt()) { // validação de entrada, se o usuário digitar um valor inválido - método boolean
-			System.out.println("A quantidade de vendas do vendedor está inválida!!");
+			System.out.println("A quantidade de vendedores está inválida!!");
 			in.next(); // irá atribuir o valor 0 
 			
 		} else {
 			
-			int qtdeVendas = in.nextInt();
-			if (qtdeVendas < 0) {
-				System.out.println("A quantidade de vendas não pode ser menor que zero!!");
+			qtdeVendedores = in.nextInt();
+			if (qtdeVendedores <= 0) {
+				System.out.println("A quantidade de vendedores está negativa ou zerada ["+ qtdeVendedores+ "]!!");
+				
 				// como o valor padrão de int é 0, não preciso atribuir nada e a qtdeVendas já ficará com esse valor
+				qtdeVendedores = 1;
 			} 
-			else {
-				vendedorElberth.qtdeVendas = qtdeVendas;
-			}
+			
 		}
 		
 		
-		vendedorElberth.imprimir();
+		/* Limpando o buffer do Scanner antes de iniciar o loop */
+		in.nextLine(); // <<< limpa o buffer antes do loop
+		
+		
 		
 		System.out.println("Finalizando...");
 		
--- src/main/java/br/edu/infnet/elberthandreapi/model/domain/Vendedor.java
+++ src/main/java/br/edu/infnet/elberthandreapi/model/domain/Vendedor.java
@@ -6,13 +6,6 @@ public class Vendedor {
 	public double salario; // valor padrão é 0.0
 	public int qtdeVendas; // valor padrão é 0
 	
-	
-	/*
-	 Classificação do vendedor - 0 a 3 vendas - Vendedor Iniciante / 4 a 7 vendas - Vendedor Junior 
-	 8 a 12 vendas - Vendedor Pleno / acima de 12 vendas - Vendedor Senior
-	 */
-	
-	
 	private double calcularBonus() {
 		// Requisitos funcionais: Bônus extra de R$ 200,00 ao salario 
 		// se ele tiver feito mais de 8 vendas e se o seu salário base for menor que R$ 2000,00
@@ -34,16 +27,19 @@ public class Vendedor {
 		// acima de 10 vendas - comissão por venda R$ 75,00
 		
 		
-		if(qtdeVendas <= 5) {
+		if(qtdeVendas >= 1 && qtdeVendas <= 5) {
 			return 30;
 		}
 		
 		else if(qtdeVendas >= 6 && qtdeVendas <= 10) {
 			return 50;
 		}
-		else { // acima de 10 vendas
+		else if (qtdeVendas > 10){ 
 			return 75;
 		}
+		
+		return 0;
+		
 		
 	}
 	
@@ -56,15 +52,57 @@ public class Vendedor {
 		return salario + comissao + bonus;
 	}
 	
+	/* Classificação do vendedor - 0 a 3 vendas - Vendedor Iniciante / 4 a 7 vendas - Vendedor Junior 
+	 8 a 12 vendas - Vendedor Pleno / acima de 12 vendas - Vendedor Senior */
+	
+	private String obterCategoria() {
+		
+		if(qtdeVendas >=0 && qtdeVendas <=3) {
+			return "Vendedor Iniciante";
+		}
+		else if(qtdeVendas >=4 && qtdeVendas <=7) {
+			return "Vendedor Junior";
+		}
+		else if(qtdeVendas >=8 && qtdeVendas <=12) {
+			return "Vendedor Pleno";
+		}
+		else {
+			return "Vendedor Senior";
+		}
+	}
+	
+	/*
+	private String obterCategoriaSwitch() {
+		
+		switch (qtdeVendas) {
+		case 0, 1, 2, 3:
+			return "Vendedor Iniciante";
+		case 4, 5, 6, 7:
+			return "Vendedor Junior";
+		case 8, 9, 10, 11, 12:
+			return "Vendedor Pleno";
+		default:
+			return "Vendedor Senior";
+		}
+	}
+	*/
 	
 	public void imprimir() { // método público, qualquer classe pode acessar esse método
 		
 		double salarioLiquido = calcularSalario();
 		
+		String categoria = obterCategoria();
+		
 		System.out.println("Valor Total = " + salarioLiquido);	
 		
-		System.out.println("O vendedor " + nome + " foi cadastrado com sucesso!!" );
+		System.out.println("O "+ categoria + " " + nome + " foi cadastrado com sucesso!!" );
 		
 	}
+	
+
+	
+	
+
+	
 	
 }
